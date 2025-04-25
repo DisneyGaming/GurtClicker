5b4885a8bc18027d7ece2a82b3cc693cccceb7b9
DisneyGaming <tommyromeolee@gmail.com>
2025-04-25 20:39:35 +0100

    Removed Console

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.identity = "Gurt Clicker"                    -- The name of the save directory (string)
     t.version = "11.5"                -- The LÖVE version this game was made for (string)
-    t.console = true                   -- Attach a console (boolean, Windows only)
+    t.console = false                   -- Attach a console (boolean, Windows only)
     t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
 
